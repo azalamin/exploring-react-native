6274a138a6b7e3c561331740388b89c58503f68e
Al-amin Sheikh <azalamins@gmail.com>
2022-06-01 02:33:56 +0600

    added rest countries api

--- components/Countries.js
+++ components/Countries.js
@@ -1,19 +1,33 @@
 import React, { useEffect, useState } from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import Country from "./Country";
 
 export default function Countries() {
-    const [countries, setCountries] = useState([]);
-    useEffect( () => {
-        fetch("https://restcountries.com/v3.1/all")
-        .then(res => res.json())
-        .then(data => setCountries(data))
-    },[])
+  const [countries, setCountries] = useState([]);
+  const [searched, setSearched] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setCountries(data);
+        setSearched(data);
+      });
+  }, []);
+
+  const handleSearch = (text) => {
+    const filtered = countries.filter((country) =>
+      country.name.common.includes(text)
+    );
+    setSearched(filtered); 
+  };
+
   return (
     <View>
-      <Text style={styles.header}>Countries: {countries?.length}</Text>
+      <Text style={styles.header}>Countries: {searched?.length}</Text>
+      <TextInput onChangeText={handleSearch} style={styles.input}></TextInput>
       <ScrollView>
-        {countries.map((country, index) => (
+        {searched.map((country, index) => (
           <Country key={index} country={country} />
         ))}
       </ScrollView>
@@ -22,9 +36,16 @@ export default function Countries() {
 }
 
 const styles = StyleSheet.create({
-    header: {
-        marginTop: 60,
-        fontSize: 40,
-        color: 'red'
-    }
-})
+  header: {
+    marginTop: 60,
+    fontSize: 40,
+    color: "red",
+  },
+  input: {
+    margin: 10,
+    height: 40,
+    color: "green",
+    padding: 10,
+    borderWidth: 1,
+  },
+});
